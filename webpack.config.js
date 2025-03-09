7f1a8bf50a3ab542a70e2ee52123e6811ce143c2
“Aneesh” <30018808+aneesh199608@users.noreply.github.com>
2025-03-09 10:56:25 +0530

    hard code restaurant basic html

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,11 @@ module.exports = {
   ],
   module: {
     rules: [
-      {
+        {
+            test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            type: "asset/resource",
+          },
+        {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
